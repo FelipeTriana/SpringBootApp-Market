55b39d1096b43901d4be7ef3c7c97ade44e7845b
FelipeTriana <felipe.trianag@udea.edu.co>
2021-08-25 12:42:03 -0500

    Connection with the database

--- src/main/java/com/platzi/market/web/controller/HolaMundoController.java
+++ src/main/java/com/platzi/market/web/controller/HolaMundoController.java
@@ -1,4 +1,4 @@
-package com.platzi.market;
+package com.platzi.market.web.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
