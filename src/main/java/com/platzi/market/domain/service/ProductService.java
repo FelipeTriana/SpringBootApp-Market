81ec0d2f40a8dffb209e0f0be3276b753a1b6bb0
FelipeTriana <felipe.trianag@udea.edu.co>
2021-11-11 14:59:29 -0500

    Add ProductService and ProductController API of products finished

---
+++ src/main/java/com/platzi/market/domain/service/ProductService.java
@@ -0,0 +1,41 @@
+package com.platzi.market.domain.service;
+
+import com.platzi.market.domain.Product;
+import com.platzi.market.domain.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> getAll() {
+        return productRepository.getAll();
+    }
+
+    public Optional<Product> getProduct(int productId) {
+        return productRepository.getProduct(productId);
+    }
+
+    public Optional<List<Product>> getByCategory(int categoryId) {
+        return productRepository.getByCategory(categoryId);
+    }
+
+    public Product save(Product product) {
+        return productRepository.save(product);
+    }
+
+    //Si el map se ejecuta es porque el producto existe, sino retorna false
+    //El map y el orElse son metodos que ofrece el Optional y es buena practica utilizarlos
+    public boolean delete(int productId) {
+        return getProduct(productId).map(product -> {
+            productRepository.delete(productId);
+            return true;
+        }).orElse(false);
+    }
+}
